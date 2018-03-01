ef7639324732373a079fee132d1110f3e1773d5e
donberke <berke.official@gmail.com>
2018-03-01 16:10:45 +0100

    base project, and gpio cpp source

---
+++ CPP_v1/src/gpio_cpp.h
@@ -0,0 +1,29 @@
+
+#include "stm32f4xx_gpio.h"
+
+/**
+  * @brief   GPIO Init structure definition
+  */
+struct GPIO {
+  uint32_t GPIO_Pin;              /*!< Specifies the GPIO pins to be configured.
+                                       This parameter can be any value of @ref GPIO_pins_define */
+
+  GPIOMode_TypeDef GPIO_Mode;     /*!< Specifies the operating mode for the selected pins.
+                                       This parameter can be a value of @ref GPIOMode_TypeDef */
+
+  GPIOSpeed_TypeDef GPIO_Speed;   /*!< Specifies the speed for the selected pins.
+                                       This parameter can be a value of @ref GPIOSpeed_TypeDef */
+
+  GPIOOType_TypeDef GPIO_OType;   /*!< Specifies the operating output type for the selected pins.
+                                       This parameter can be a value of @ref GPIOOType_TypeDef */
+
+  GPIOPuPd_TypeDef GPIO_PuPd;     /*!< Specifies the operating Pull-up/Pull down for the selected pins.
+                                       This parameter can be a value of @ref GPIOPuPd_TypeDef */
+// functions:
+
+  void init() {
+	  GPIOA->AFR = (volatile uint32_t)1;
+  }
+
+};
+
